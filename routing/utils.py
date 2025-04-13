725fd3ec52e561275d123089e7624d3f9f350d01
lintang <lintang@sutawika.com>
2025-04-13 16:09:11 -0400

    fix answwer capture sequence

--- routing/utils.py
+++ routing/utils.py
@@ -14,9 +14,9 @@ def preprocess_routing(x, state, pl_system_message, nl_system_message):
     current_step = state["current_step"]
     solve_with = state["step"][current_step-1]["output"][0].split("\n")[0]
     if solve_with == "programming language":
-        state["system_message"] = pl_system_message
+        state["user_message"] = pl_system_message
     elif solve_with == "natural language":
-        state["system_message"] = nl_system_message
+        state["user_message"] = nl_system_message
     return x, state
 
 def postprocess_routing(x, state):
@@ -26,7 +26,7 @@ def postprocess_routing(x, state):
         x = is_runnable_code(x) 
     elif solve_with == "natural language":
         try:
-            x = x.split("answer is")[-1].strip()
+            x = x.split("Answer:")[-1].strip()
         except:
             pass
     return x, state
