3b2a159ab7c3839d7d32c3f597ed6331312c2987
lintang <lintang@sutawika.com>
2025-04-13 16:09:28 -0400

    remove unused lines

--- routing/tasks/route.py
+++ routing/tasks/route.py
@@ -9,10 +9,7 @@ from routing.utils import (
 @register_task("route00")
 class Route00(YevalTask):
     user_message=lambda x: f"{x}\n\nWhich method is the best way to solve this problem?"
-    output_text=lambda x: "programming language"
     sampling_args={"stop": ["\n\n", "\n"], "extra_body":{"guided_choice": ["programming language", "natural language"]}}
-    evaluation={"accuracy": match_routing}
-    # logging=log_token_usage
 
 @register_task("route01")
 class Route01(Route00):
