23fb3c56b2bf778796b619c07e9dd24db69c7671
lintang <lintang@sutawika.com>
2025-04-15 16:24:48 -0400

    rename prompts

--- routing/prompt_routing_direct.py
+++ routing/prompt_routing_direct.py
@@ -1,6 +1,6 @@
 from yeval.prompt import YevalPrompt, register_prompt
 
-@register_prompt("direct_NL_00")
+@register_prompt("direct_00")
 class SelectNL00(YevalPrompt):
     user_message=lambda x: f"""\
 Choose only one way to solve the problem: by thinking step-by-step OR writing a program as a way to solve a given task. Do NOT use both:
@@ -9,7 +9,7 @@ Choose only one way to solve the problem: by thinking step-by-step OR writing a 
 {x}
 """
 
-@register_prompt("direct_PL_00")
+@register_prompt("direct_01")
 class SelectPL00(YevalPrompt):
     user_message=lambda x: f"""\
 Choose only one way to solve the problem: by thinking step-by-step OR writing a program as a way to solve a given task. Do NOT use both:
@@ -18,17 +18,19 @@ Choose only one way to solve the problem: by thinking step-by-step OR writing a 
 {x}
 """
 
-@register_prompt("direct_NL_01")
+@register_prompt("direct_02")
 class SelectNL00(YevalPrompt):
-    system_message="""\
+    system_message=lambda x: f"""\
+{x}
 Choose only one way to solve the problem: by thinking step-by-step OR writing a program as a way to solve a given task. Do NOT use both:
 1. Thinking step-by-step: Think step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, you MUST write the answer after 'The answer is'.
 2. Writing a program: DIRECTLY and ONLY write a program with the PYTHON programming language. The function must be named solution() without any input arguments. At the end, you MUST return an single value.
 """
 
-@register_prompt("direct_PL_01")
+@register_prompt("direct_03")
 class SelectPL00(YevalPrompt):
-    system_message="""\
+    system_message=lambda x: f"""\
+{x}
 Choose only one way to solve the problem: by thinking step-by-step OR writing a program as a way to solve a given task. Do NOT use both:
 1. Writing a program: DIRECTLY and ONLY write a program with the PYTHON programming language. The function must be named solution() without any input arguments. At the end, you MUST return an single value.
 2. Thinking step-by-step: Think step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, you MUST write the answer after 'The answer is'.
