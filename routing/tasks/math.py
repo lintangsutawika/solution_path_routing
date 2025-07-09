20cced91013fd1c674b4adda039691ea3aedc28e
lintang <lintang@sutawika.com>
2025-07-08 23:38:25 -0400

    add math500

--- routing/tasks/math.py
+++ routing/tasks/math.py
@@ -1,6 +1,9 @@
 from yeval.task.hendrycks_math import MATHBaseTask
 from yeval.task import register_task, YevalTask
 
+from yeval.log.usage import log_token_usage
+from yeval.metrics import math_eval
+
 @register_task("train:math_algebra")
 class TrainMATHAlgebraTask(MATHBaseTask):
     preprocessing=None
@@ -30,3 +33,39 @@ class TrainMATHPrealgebraTask(TrainMATHAlgebraTask):
 @register_task("train:math_precalculus")
 class TrainMATHPrecalculusTask(TrainMATHAlgebraTask):
     data_name='precalculus'
+
+class MATH500BaseTask(YevalTask):
+    data_path="HuggingFaceH4/MATH-500"
+    input_text=lambda x: x["problem"]
+    output_text=lambda x: x["answer"]
+    test_split="test"
+    evaluation={"accuracy": math_eval}
+    logging=log_token_usage
+
+@register_task("math500_algebra")
+class MATH500AlgebraTask(MATH500BaseTask):
+    preprocessing=lambda dataset: dataset.filter(lambda x: x["subject"] == "Algebra")
+
+@register_task("math500_counting_and_probability")
+class MATH500CountingAndProbabilityTask(MATH500BaseTask):
+    preprocessing=lambda dataset: dataset.filter(lambda x: x["subject"] == "Counting & Probability")
+
+@register_task("math500_geometry")
+class MATH500GeometryTask(MATH500BaseTask):
+    preprocessing=lambda dataset: dataset.filter(lambda x: x["subject"] == "Geometry")
+
+@register_task("math500_intermediate_algebra")
+class MATH500IntermediateAlgebraTask(MATH500BaseTask):
+    preprocessing=lambda dataset: dataset.filter(lambda x: x["subject"] == "Intermediate Algebra")
+
+@register_task("math500_number_theory")
+class MATH500NumberTheoryTask(MATH500BaseTask):
+    preprocessing=lambda dataset: dataset.filter(lambda x: x["subject"] == "Number Theory")
+
+@register_task("math500_prealgebra")
+class MATH500PrealgebraTask(MATH500BaseTask):
+    preprocessing=lambda dataset: dataset.filter(lambda x: x["subject"] == "Prealgebra")
+
+@register_task("math500_precalculus")
+class MATH500PrecalculusTask(MATH500BaseTask):
+    preprocessing=lambda dataset: dataset.filter(lambda x: x["subject"] == "Precalculus")
